104f6925eed5d972b0a4d8473413687d7184682a
num-con <146140810+givd-bid@users.noreply.github.com>
2023-11-03 09:41:45 -0700

    Update write.js

--- random-scripts/write.js
+++ random-scripts/write.js
@@ -1,6 +1,6 @@
 var x = window.open("about:blank");
-x.document.addEventListener("click", function() {
+x.document.addEventListener("click", c);
+function x() {
   x.close()
 }
-                           )
 x.document.close();
