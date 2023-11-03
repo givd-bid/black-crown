614799e1ced71b2cdf37a49501769191372d2b22
num-con <146140810+givd-bid@users.noreply.github.com>
2023-11-03 09:33:03 -0700

    Create write.js

---
+++ random-scripts/write.js
@@ -0,0 +1,19 @@
+var x = window.open("about:blank");
+var html = `
+<head>
+  <title>wow haha</title>
+</head>
+<body>
+  <div>
+    <button onclick = "func()">HHAHAHAHAHHAHAHAHAHHAHA</button>
+    <script>
+       function func() {
+            window.close()
+            }
+    </script>
+  </div>
+</body>
+
+`;
+x.document.write(html);
+x.document.close();
