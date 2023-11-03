60ff5b02df35a881927b5ce9af99544caa963475
num-con <146140810+givd-bid@users.noreply.github.com>
2023-11-03 09:40:28 -0700

    Update write.js

--- random-scripts/write.js
+++ random-scripts/write.js
@@ -1,12 +1,6 @@
 var x = window.open("about:blank");
-var html = `
-<head>
-  <title>wow haha</title>
-</head>
-
-`;
-x.document.write(html);
-x.document.addEventListener("click", function () {
+x.document.write('<head><title>wow haha</title></head>');
+x.document.addEventListener("click", function() {
   x.close()
 }
                            )
